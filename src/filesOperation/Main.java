4f5d08ace481504657c35ee7b4bf092c8acb449c
NorwegianKitty <next.level@internet.ru>
2024-09-18 13:43:42 +0700

    rdy

--- src/filesOperation/Main.java
+++ src/filesOperation/Main.java
@@ -5,10 +5,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String[] testArr = {"-f", "-p", "sample-", "in1.txt"};
-		
-		args = testArr;
-		
 		ArgsHandler argsHandler = new ArgsHandler(args);	
 		
 		try {
